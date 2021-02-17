a9dc7074342f59b5207d343c70301538a915a63f
LucasPollet <lucaspol@hotmail.fr>
2021-02-17 11:08:46 +0100

    Implement search bar

--- src/index.js
+++ src/index.js
@@ -5,40 +5,52 @@ import regeneratorRuntime from "regenerator-runtime";
   
   const description = document.createElement('p');
   description.innerHTML = "PlantSearcher est un moteur de recherche référencant des milliers de plantes";
-  
+ 
   const input_value = document.createElement('input');
   input_value.setAttribute('type', 'text');
-  input_value.setAttribute('onclick', 'search()');
+  input_value.setAttribute('id', 'input_id');
 
   const button = document.createElement('input');
   button.setAttribute('type', 'button');
   button.setAttribute('id', 'buttid');
+  button.addEventListener("click", search);
   button.value = 'Chercher';
+
+  const plant = document.createElement('h2');
+  plant.setAttribute('id', 'result');
 
   document.body.appendChild(title);
   document.body.appendChild(description); 
   document.body.appendChild(input_value);
   document.body.appendChild(button);
-
-  let requestURL = 'https://trefle.io/api/v1/plants/search?token=Fh5DUrCLASPWC00uquG6RhbQ3uluXJw5wSNyjyqU1Xs&q=dandelion&limit=5';
-  
-  const fetch = require('node-fetch');
-
-  (async () => {
-    const response = await fetch(requestURL);
-    const myjson = await response.json();
-    
-    let text = '';
-    for(let i=0; i<myjson.data.length; i++){
-      text += "<a href=''> "+myjson.data[i].common_name+"</a><br><img height='300' src='"+myjson.data[i].image_url+"'><br>";
-    }
-
-    const plant = document.createElement('h2');
-    plant.innerHTML = text; 
-
-    document.body.appendChild(plant);
-  })();
+  document.body.appendChild(plant);
 
  function search(){
-    let buttvalue = document.getElementById('buttid').value;
+    const plant = document.createElement('h2');
+    plant.setAttribute('id', 'result');
+
+    let butt = document.getElementById('input_id').value;
+
+    let requestURL = 'https://trefle.io/api/v1/plants/search?token=Fh5DUrCLASPWC00uquG6RhbQ3uluXJw5wSNyjyqU1Xs&q='+butt+'&limit=5';
+  
+    const fetch = require('node-fetch');
+  
+    (async () => {
+      const response = await fetch(requestURL);
+      const myjson = await response.json();
+      
+      let text = '';
+      for(let i=0; i<myjson.data.length; i++){
+        if(myjson.data[i].image_url == null){
+          text += "<a href=''> "+myjson.data[i].common_name+"</a><br>Pas d'image pour cette espèce :/<br>";
+        }else{
+          text += "<a href=''> "+myjson.data[i].common_name+"</a><br><img height='300' src='"+myjson.data[i].image_url+"'><br>";
+        }
+      }
+
+      plant.innerHTML = text; 
+  
+      document.body.removeChild(document.getElementById('result'));
+      document.body.appendChild(plant);
+    })();
  }
